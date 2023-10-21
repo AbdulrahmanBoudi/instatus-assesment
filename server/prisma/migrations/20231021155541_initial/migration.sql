f02f2d47be0389ed83630b06ec55f0165888b667
aboudi <aboudi@sumerge.com>
2023-10-21 19:28:57 +0300

    added tsc

--- server/prisma/migrations/20231021155541_initial/migration.sql
+++ server/prisma/migrations/20231021155541_initial/migration.sql
@@ -5,10 +5,6 @@ BEGIN
     RETURN concat(prefix, gen_random_uuid());
 END;
 $$ LANGUAGE PLPGSQL VOLATILE;
-
--- CreateEnum
-CREATE TYPE "Actions" AS ENUM ('LOGGED_IN');
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL DEFAULT prefix_uuid('user_'::text),
@@ -25,10 +21,12 @@ CREATE TABLE "Events" (
     "object" TEXT NOT NULL,
     "actor_id" TEXT NOT NULL,
     "actor_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "group" TEXT NOT NULL,
     "action_id" TEXT NOT NULL,
-    "target_id" TEXT NOT NULL,
-    "target_name" TEXT NOT NULL,
+    "action_name" TEXT NOT NULL,
+    "target_id" TEXT,
+    "target_name" TEXT,
     "location" TEXT NOT NULL,
     "occured_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "metadata_id" TEXT NOT NULL,
@@ -38,9 +36,9 @@ CREATE TABLE "Events" (
 
 -- CreateTable
 CREATE TABLE "Action" (
-    "id" TEXT NOT NULL DEFAULT prefix_uuid('user_'::text),
+    "id" TEXT NOT NULL DEFAULT prefix_uuid('evt_act_'::text),
     "object" TEXT NOT NULL,
-    "name" "Actions",
+    "name" TEXT NOT NULL,
 
     CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
 );
@@ -55,13 +53,28 @@ CREATE TABLE "Metadata" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_id_name_email_key" ON "User"("id", "name", "email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_name_key" ON "User"("id", "name");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Events_metadata_id_key" ON "Events"("metadata_id");
 
--- AddForeignKey
-ALTER TABLE "Events" ADD CONSTRAINT "Events_actor_id_fkey" FOREIGN KEY ("actor_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Action_name_key" ON "Action"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Action_id_name_key" ON "Action"("id", "name");
 
 -- AddForeignKey
-ALTER TABLE "Events" ADD CONSTRAINT "Events_action_id_fkey" FOREIGN KEY ("action_id") REFERENCES "Action"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Events" ADD CONSTRAINT "Events_actor_id_actor_name_email_fkey" FOREIGN KEY ("actor_id", "actor_name", "email") REFERENCES "User"("id", "name", "email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Events" ADD CONSTRAINT "Events_action_id_action_name_fkey" FOREIGN KEY ("action_id", "action_name") REFERENCES "Action"("id", "name") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Events" ADD CONSTRAINT "Events_target_id_target_name_fkey" FOREIGN KEY ("target_id", "target_name") REFERENCES "User"("id", "name") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Events" ADD CONSTRAINT "Events_metadata_id_fkey" FOREIGN KEY ("metadata_id") REFERENCES "Metadata"("x_request_id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/src/server.ts
+++ server/src/server.ts
@@ -13,9 +13,5 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/test", router);
-// app.get("/", async (req, res) => {
-//   const users = await prisma.user.findMany();
-//   res.json(users);
-// });
 
 app.listen(PORT, () => console.log(`server is up on port ${PORT}`));
